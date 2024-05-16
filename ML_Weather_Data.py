3bb0d229e075b8ecc00204d6bd927f9d3ddbb287
Knobloch-design <alec-knobloch@uiowa.edu>
2024-05-16 17:19:05 -0500

    cleaning data in progess

--- ML_Weather_Data.py
+++ ML_Weather_Data.py
@@ -3,7 +3,8 @@ import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-
+import seaborn as sns
+import re
 # Path to the CSV file
 csv_file_path = r"C:\Users\aleck\OneDrive\Documents\GitHub\Weather\Weather_Data.csv"
 df = pd.read_csv(csv_file_path)
@@ -11,7 +12,7 @@ df = pd.read_csv(csv_file_path)
 def read_csv_to_numpy(csv_file_path):
     try:
         # Read the CSV file into a pandas DataFrame
-        df = pd.read_csv(csv_file_path)
+        
 
         # Convert DataFrame to a NumPy array
         data_array = df.to_numpy()
@@ -54,8 +55,8 @@ def remove_columns_with_majority_null(data_array):
 
     for x in columns_to_remove:
         removed_columns= removed_columns+", "+ x
-    print("columns_to_remove: ", columns_to_remove)
-    print("columns_to_remove_length: ", len(columns_to_remove))
+    #print("columns_to_remove: ", columns_to_remove)
+    #print("columns_to_remove_length: ", len(columns_to_remove))
     # Remove columns with high null percentage
     cleaned_df = df.drop(columns=columns_to_remove)
 
@@ -71,31 +72,77 @@ print("Original data array:")
 
 # Remove columns with majority null values
 cleaned_data_array = remove_columns_with_majority_null(df)
-print(cleaned_data_array.columns)
+#print(cleaned_data_array.columns)
+
 #print(cleaned_data_array.describe())
 #print(cleaned_data_array.head())
-#keyFeatures = ['DATE', 'DailyAverageWindSpeed','DailyPeakWindSpeed', 'DailyPrecipitation','DailySustainedWindSpeed','HourlyDewPointTemperature',
-#                'HourlyPrecipitation', 'HourlyPresentWeatherType','HourlyDryBulbTemperature','HourlyWetBulbTemperature',
-#               'HourlyRelativeHumidity', 'HourlySkyConditions', 'HourlyWindGustSpeed', 'HourlyWindSpeed','Sunrise', 'Sunset' ]
+keyFeatures = ['DATE', 'DailyAverageWindSpeed','DailyPeakWindSpeed', 'DailyPrecipitation','DailySustainedWindSpeed','HourlyDewPointTemperature',
+                'HourlyPrecipitation', 'HourlyPresentWeatherType','HourlyDryBulbTemperature','HourlyWetBulbTemperature',
+                'HourlyRelativeHumidity', 'HourlySkyConditions', 'HourlyWindGustSpeed', 'HourlyWindSpeed','Sunrise', 'Sunset' ]
 
+keyFeatures = pd.DataFrame(cleaned_data_array[keyFeatures])
+#print(keyFeatures.head())
+#print(cleaned_data_array.head())
 #keyData = cleaned_data_array[keyFeatures]
-x = cleaned_data_array['DATE']
+
+
 #print(keyData.describe())
-y= cleaned_data_array['HourlyVisibility']
+
+#TODO Hourly visibility and possibly other data columns have '*' entries and 
+#other characters randomly throughout dataset
+y = cleaned_data_array['HourlyVisibility']
+
+y.astype('str')
+
+for i in range(len(y)):
+
+    if y.loc[i] == '*':
+         cleaned_data_array.loc[i,'HourlyVisibility'] = '0.0'
+    if bool(re.search('[a-zA-Z]', y.loc[i])):
+        re.sub(cleaned_data_array.loc[i,'HourlyVisibility'], '', '[a-zA-Z]')
+
+
+
+y = pd.DataFrame(y[:100])
+
+try:
+    y = pd.to_numeric(y,errors='coerce')
+except Exception as e:
+    print(f"Error: {e}")
+
+y = y.dropna()
+
+y = y['HourlyVisibility']
+print("type y: ", type(y))
+x = cleaned_data_array['DATE']
+x = x[:len(y)]
+for n in y:
+    print("type: ",type(n))
+print("y: ",y)
+print("x: ",x)
+"""
 a = cleaned_data_array['HourlyDryBulbTemperature']
 b = cleaned_data_array['HourlyWetBulbTemperature']
+"""
 
 
+#y = [i for i in range(10)]
 fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
-print(a)
 
-ax.plot(x, x, label='HourlyDryBulbTemperature')  # Plot some data on the axes.
+
+
+
+
+
+ax.plot(x, y, label='HourlyVisibility')  # Plot some data on the axes.
 #ax.plot(x, b, label='HourlyWetBulbTemperature')  # Plot more data on the axes...
 ax.set_xlabel('Time')  # Add an x-label to the axes.
 ax.set_ylabel('Temperature')  # Add a y-label to the axes.
 ax.set_title("Simple Plot")  # Add a title to the axes.
 ax.legend()  # Add a legend.
-fig.show()
+plt.show()
+
+
 #DecisionTreeRegressor does not work with time variables
 #model = DecisionTreeRegressor(random_state=1)
 """model.fit(keyData, y)
